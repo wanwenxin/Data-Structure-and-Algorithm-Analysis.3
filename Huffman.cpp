ef4edada15d1e9a2be698fa5f7eea4044ae0e9e3
wanwenxin <33198921+wanwenxin@users.noreply.github.com>
2017-11-27 13:11:55 +0800

    Create Huffman.cpp

---
+++ Huffman.cpp
@@ -0,0 +1,18 @@
+// Huffman.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"readname.h"
+#include"command.h"
+
+int main()
+{
+	while (1)
+	{
+		cout << "1.huffman compress" << endl;
+		cout << "2.huffman decompress" << endl;
+		cout << "3.exit" << endl;
+		if(command()==1) continue; //执行command()，返回1，则输入指令错误
+	}
+	return 0;
+}
