6d62ae658a0d6d26d09f97d95fa50a5302c18840
wanwenxin <33198921+wanwenxin@users.noreply.github.com>
2017-11-27 13:13:40 +0800

    Create node.h

---
+++ node.h
@@ -0,0 +1,20 @@
+#pragma once
+#define N 26                //叶子总数,即需编码字符的个数  
+#define M 2*N-1             //节点总数  
+
+typedef struct HuffmanTree//用于存储Huffman树中的节点信息  
+{
+	char ch;         //字符  
+	int weight;      //权值  
+	int Lchild;      //左孩子  
+	int Rchild;      //右孩子  
+	int parent;      //父节点  
+}HuffmanTree;
+
+
+typedef struct CodeType//用于存储单个字符的编码结果  
+{
+	char ch;        //字符  
+	char bits[N];   //位串  
+	int start;      //编码在位串中的起始位置  
+}CodeType;
